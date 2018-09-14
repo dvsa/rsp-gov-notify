77ab7db7e45eb977113db41688c068864510ace3
John Morley <john.morley@bjss.com>
2018-09-14 10:49:01 +0100

    Remove unused authorizer function

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,7 @@
-import auth from './functions/auth';
 import notifyEmail from './functions/notifyEmail';
 import notifySms from './functions/notifySms';
 
 const handler = {
-	auth,
 	notifyEmail,
 	notifySms,
 };
