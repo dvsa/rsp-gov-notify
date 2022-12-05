0876fc44fd35ff80c39cc353f3a9b4a25799cfd4
Anna Dodson <Anna.Dodson@DVSA.gov.uk>
2022-12-05 11:09:18 +0000

    Add extra debug logs into end SMS & email service
    - Error thrown when logging out message id from Notify library
    due to changes in Notify client library
    - Updated the error handling as per the new version of Notify client
    - New tests for error logging and returning error messages to the user
    - Fix for double zip appended to file uploads in GitHub action
    - Ticket RSP-2103 & RSP-2104

--- src/test/services/notifyService.unit.js
+++ src/test/services/notifyService.unit.js
@@ -2,13 +2,14 @@
 import expect from 'expect';
 import sinon from 'sinon';
 import { NotifyClient } from 'notifications-node-client';
-
+import * as logger from '../../utils/logger';
 import config from '../../utils/config';
 import NotifyService from '../../services/notifyService';
 import TemplateKeySelector from '../../utils/TemplateKeySelector';
 
 describe('notifyService', () => {
 	context('notifyEmail', () => {
+		let logStub;
 		let sendEmailStub;
 		let templateKeySelectorStub;
 		let personalisation;
@@ -24,7 +25,7 @@ describe('notifyService', () => {
 		};
 
 		beforeEach(() => {
-			sinon.stub(console, 'log');
+			logStub = sinon.stub(logger, 'logInfo');
 			personalisation = personalisationWithLink('https://portal.local/47bmo7p9syg');
 			sendEmailStub = sinon.stub(NotifyClient.prototype, 'sendEmail');
 			templateKeySelectorStub = sinon.stub(TemplateKeySelector.prototype, 'keyForEmail');
@@ -37,6 +38,7 @@ describe('notifyService', () => {
 			TemplateKeySelector.prototype.keyForEmail.restore();
 			config.paymentPortalUrl.restore();
 			config.notifyApiKey.restore();
+			logStub.restore();
 		});
 
 		context('when called with email, full payload for English language and respond', () => {
@@ -46,21 +48,29 @@ describe('notifyService', () => {
 					.returns('ENGLISH_EMAIL_TEMPLATE_KEY');
 				sendEmailStub
 					.withArgs('ENGLISH_EMAIL_TEMPLATE_KEY', emailAddr, personalisation)
-					.resolves({ body: { id: 'gov-notify-message-id' } });
+					.resolves({ data: { id: 'gov-notify-message-id' } });
 			});
 			it('should call the notify client correctly then respond', async () => {
 				const response = await NotifyService.email(emailAddr, payload);
 				sinon.assert.calledWith(sendEmailStub, 'ENGLISH_EMAIL_TEMPLATE_KEY', emailAddr, personalisation);
 				expect(response).toMatchObject({ statusCode: 200 });
 			});
+			it('should call the notify client and log the message id', async () => {
+				await NotifyService.email(emailAddr, payload);
+				sinon.assert.calledWith(logStub, 'SendEmailSuccess', { notifyMessageId: 'gov-notify-message-id' });
+			});
 		});
 	});
 
 	context('notifySms', () => {
+		let logInfoStub;
+		let logErrorStub;
 		let sendSmsStub;
 		let templateKeySelectorStub;
 
 		beforeEach(() => {
+			logInfoStub = sinon.stub(logger, 'logInfo');
+			logErrorStub = sinon.stub(logger, 'logError');
 			sendSmsStub = sinon.stub(NotifyClient.prototype, 'sendSms');
 			templateKeySelectorStub = sinon.stub(TemplateKeySelector.prototype, 'keyForSms');
 			sinon.stub(config, 'paymentPortalUrl').returns('https://portal.local');
@@ -72,6 +82,8 @@ describe('notifyService', () => {
 			TemplateKeySelector.prototype.keyForSms.restore();
 			config.paymentPortalUrl.restore();
 			config.notifyApiKey.restore();
+			logInfoStub.restore();
+			logErrorStub.restore();
 		});
 
 		context('when called with phone number, full payload for English language and respond', () => {
@@ -84,12 +96,93 @@ describe('notifyService', () => {
 					.returns('ENGLISH_SMS_TEMPLATE_KEY');
 				sendSmsStub
 					.withArgs('ENGLISH_SMS_TEMPLATE_KEY', '12345', personalisation)
-					.resolves({ body: { id: 'gov-notify-message-id' } });
+					.resolves({ data: { id: 'gov-notify-message-id' } });
 			});
 			it('should call the notify client correctly then respond', async () => {
 				const response = await NotifyService.sms('12345', smsPayloadForLanguage('en'));
 				sinon.assert.calledWith(sendSmsStub, 'ENGLISH_SMS_TEMPLATE_KEY', '12345', personalisation);
 				expect(response).toMatchObject({ statusCode: 200 });
+			});
+			it('should call the notify client and log the message id', async () => {
+				await NotifyService.sms('12345', smsPayloadForLanguage('en'));
+				sinon.assert.calledWith(logInfoStub, 'SendSmsSuccess', { notifyMessageId: 'gov-notify-message-id' });
+			});
+		});
+
+		context('when error is returned, it is handled correctly', () => {
+			let personalisation;
+
+			beforeEach(() => {
+				personalisation = personalisationWithLink('https://portal.local/47bmo7p9syg');
+				templateKeySelectorStub
+					.withArgs('en')
+					.returns('ENGLISH_SMS_TEMPLATE_KEY');
+				sendSmsStub
+					.withArgs('ENGLISH_SMS_TEMPLATE_KEY', '12345', personalisation)
+					.rejects({
+						response: {
+							data: {
+								status_code: 400,
+								errors: [{
+									error: 'BadRequestError',
+									message: "Can't send to this recipient using a team-only API key",
+								}],
+							},
+						},
+					});
+			});
+			it('should fail to send through the notify client and return the error', async () => {
+				const response = await NotifyService.sms('12345', smsPayloadForLanguage('en'));
+				expect(response).toMatchObject({
+					statusCode: 400,
+					body: JSON.stringify({
+						statusCode: 400,
+						errors: [{
+							error: 'BadRequestError',
+							message: "Can't send to this recipient using a team-only API key",
+						}],
+					}),
+				});
+			});
+			it('should call the notify client and call the error logger', async () => {
+				await NotifyService.sms('12345', smsPayloadForLanguage('en'));
+				sinon.assert.calledWith(logErrorStub, 'SendSmsError', {
+					notifyApiError: {
+						statusCode: 400,
+						errors: [
+							{
+								error: 'BadRequestError',
+								message: "Can't send to this recipient using a team-only API key",
+							},
+						],
+					},
+				});
+			});
+		});
+
+		context('when an unexpected error occurs, it is handled correctly', () => {
+			let personalisation;
+
+			beforeEach(() => {
+				personalisation = personalisationWithLink('https://portal.local/47bmo7p9syg');
+				templateKeySelectorStub
+					.withArgs('en')
+					.returns('ENGLISH_SMS_TEMPLATE_KEY');
+				sendSmsStub
+					.withArgs('ENGLISH_SMS_TEMPLATE_KEY', '12345', personalisation)
+					.rejects(new Error('Something unexpected happened'));
+			});
+			it('should fail with unexpected error and handle error gracefully to the user', async () => {
+				const response = await NotifyService.sms('12345', smsPayloadForLanguage('en'));
+				expect(response).toMatchObject({
+					statusCode: 400,
+					body: JSON.stringify({
+						errors: [{
+							error: 'RSPError',
+							message: 'An unexpected error occurred. Something unexpected happened',
+						}],
+					}),
+				});
 			});
 		});
 
